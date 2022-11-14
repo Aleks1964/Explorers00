ff03ba70b434d3569d133ba1f47a0ecaf8f2960c
Aleksandr <sveta-sasha-21@yandex.ru>
2022-11-15 00:22:06 +0300

    Example 003 Sum Progect + Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,9 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 ... 9
+int numberB = new Random().Next(1, 10);
 int rezult = numberA + numberB;
+Console.Write("Число А = ");
+Console.WriteLine(numberA);
+Console.Write("Число B = ");
+Console.WriteLine(numberB);
+Console.Write("Сумма А + B = ");
 Console.WriteLine(rezult);
